147be3c03f60b50f67155f7db56c471e089126d1
Fatima <fatima.machhouri1@gmail.com>
2019-10-28 18:58:28 +0100

    [WIP] Contact page

---
+++ src/components/contactSheet.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import '../style/contactSheet.css';
+
+interface IState {
+    nameContact?: string;
+    positionContact: string;
+    emailContact: string;
+}
+
+interface IProps { 
+    nameContact: string;
+    positionContact: string;
+    emailContact: string;
+}
+
+class ContactSheet extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      nameContact: this.props.nameContact,
+      positionContact: this.props.positionContact,
+      emailContact: this.props.emailContact,
+    };
+  }
+  
+  render() {
+    return (
+      <div className="container-contactSheet">
+        <p className="name-contact">{this.state.nameContact}</p>
+        <p className="position-contact">{this.state.positionContact}</p>
+        <p className="email-contact">{this.state.emailContact}</p>
+      </div>
+    );
+  }
+    
+}
+
+export default ContactSheet;
