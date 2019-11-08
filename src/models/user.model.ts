397f72f895e6c436fdb6f862c97385bab77f45c7
MartinCayuelas <martincayuelas@gmail.com>
2019-11-08 11:10:48 +0100

    [Add] login function

---
+++ src/models/user.model.ts
@@ -0,0 +1,8 @@
+
+class User {
+    public idUser!: number;
+    public login!: string;
+    public password!: string;
+};
+
+export default User;
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,6 +1,24 @@
+import axios from 'axios';
+import User from '../models/user.model';
+
+const login = async (user: User) => {
+  try {
+    const res = await axios
+      .post('api/utilisateur/connexion', {
+        login: user.login,
+        password: user.password
+      });
+    localStorage.setItem('tokenFormatech', res.data);
+    return res.data;
+  }
+  catch (err) {
+    console.log(err);
+  }
+};
+
 const logout = () => {
- // remove user from local storage to log user out
- localStorage.removeItem('tokenFormatech');
+  // remove user from local storage to log user out
+  localStorage.removeItem('tokenFormatech');
 }
 
-export { logout };
+export { login, logout };
