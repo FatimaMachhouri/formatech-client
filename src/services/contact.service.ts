919835f16c6389babaaf1839847659df92da7f51
MartinCayuelas <martincayuelas@gmail.com>
2019-11-13 09:59:12 +0100

    [Add] JWT for Contact-Ig-Do-Home

--- src/services/contact.service.ts
+++ src/services/contact.service.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import * as config from '../config/configApi';
+import { getToken } from './token.service';
 
 export const getContactElements = async () => {
   try {
@@ -12,9 +13,11 @@ export const getContactElements = async () => {
 };
 
 export const addContactElement = async (newContact: { name: string, email: string, position: string }) => {
-  //const token = localStorage.getItem('usertoken');
+  const token = getToken();
   try {
-    await axios.post(`${config.API_URL}/contact`, newContact);
+    await axios.post(`${config.API_URL}/contact`, newContact,{
+      headers: { Authorization: `Bearer ${token}` }
+    });
   }
   catch (error) {
     throw error.response;
@@ -22,9 +25,11 @@ export const addContactElement = async (newContact: { name: string, email: strin
 };
 
 export const updateElementInContact = async (contactElement: { idContact: number, name: string, email: string, position: string }) => {
-  //const token = localStorage.getItem('usertoken');
+  const token = getToken();
   try {
-    await axios.put(`${config.API_URL}/contact/modifier/${contactElement.idContact}`, contactElement);
+    await axios.put(`${config.API_URL}/contact/modifier/${contactElement.idContact}`, contactElement,{
+      headers: { Authorization: `Bearer ${token}` }
+    });
   }
   catch (error) {
     throw error.response;
@@ -32,8 +37,11 @@ export const updateElementInContact = async (contactElement: { idContact: number
 };
 
 export const deleteElementInContact = async (idContact: number) => {
+  const token = getToken();
   try {
-    await axios.delete(`${config.API_URL}/contact/supprimer/${idContact}`);
+    await axios.delete(`${config.API_URL}/contact/supprimer/${idContact}`,{
+      headers: { Authorization: `Bearer ${token}` }
+    });
   }
   catch (error) {
     throw error.response;
