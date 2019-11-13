3ee643ad045c5b780ce0d9030a47b19411ee71c4
Lucas Goncalves <lucas.pro@mail.com>
2019-11-13 23:08:25 +0100

    [WIP] Editable Preview

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -12,6 +12,8 @@ interface IState {
   mainText: string;
   title: string;
   idHome: number;
+  previewOne: HomeElem;
+  previewTwo: HomeElem;
 }
 interface IProps {
 }
@@ -23,7 +25,9 @@ class Root extends React.Component<IProps, IState> {
       connected: false,
       mainText: '',
       title: '',
-      idHome: 0
+      idHome: 0,
+      previewOne: new HomeElem(),
+      previewTwo: new HomeElem()
     };
 
     const issues = verifyToken();
@@ -34,20 +38,60 @@ class Root extends React.Component<IProps, IState> {
     const pageContent = getHomeElements();
     pageContent.then((allElements: HomeElem[]) => {
       if (allElements !== undefined) {
+        console.log(allElements)
         this.setState({
           mainText: allElements[0]!.content,
           title: allElements[0]!.title,
-          idHome: allElements[0]!.idHome
+          idHome: allElements[0]!.idHome,
         });
+        const elem1 = new HomeElem()
+        elem1.content = allElements[1]!.content;
+        elem1.idHome = allElements[1]!.idHome;
+        elem1.media = allElements[1]!.media;
+        elem1.title = allElements[1]!.title;
+
+        const elem2 = new HomeElem()
+        elem2.content = allElements[2]!.content;
+        elem2.idHome = allElements[2]!.idHome;
+        elem2.media = allElements[2]!.media;
+        elem2.title = allElements[2]!.title;
+
+        this.setState({
+          previewOne: elem1,
+          previewTwo: elem2
+        });
+
       }
 
     });
 
+    // Function for the page
     this.renderText = this.renderText.bind(this);
     this.save = this.save.bind(this);
     this.showSavedButton = this.showSavedButton.bind(this);
     this.changeTitle = this.changeTitle.bind(this);
+
+    // Functions to handle changes on Preview Components: 
+    this.changePreviewOne = this.changePreviewOne.bind(this)
+    this.changePreviewTwo = this.changePreviewTwo.bind(this)
+
+
   }
+
+  changePreviewOne(elem: HomeElem) {
+    this.setState({
+      previewOne: elem
+    });
+  }
+
+  changePreviewTwo(elem: HomeElem) {
+    this.setState({
+      previewOne: elem
+    });
+  }
+
+
+
 
   renderText() {
     if (this.state.connected) {
@@ -91,8 +135,8 @@ class Root extends React.Component<IProps, IState> {
         <MainTitle name={this.state.title} connected={this.state.connected} action={this.changeTitle}/>
         {this.renderText()}
         <div className="pres-formation">
-          <Preview name="Développement Opérationnel" className="do" />
-          <Preview name="Informatique et Gestion" className="ig" />
+          <Preview elem={this.state.previewOne} connected={this.state.connected}  className="do" />
+          <Preview elem={this.state.previewTwo} connected={this.state.connected}  className="ig" />
         </div>
       </div>
     );
